45514f170b3acf994ffb63232e3f6c5819999135
jorted-runner <jorted.runner1996@gmail.com>
2024-03-09 21:42:24 -0600

    images

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,8 +3,24 @@ const directoryDataURL = "https://jorted-runner.github.io/wdd230/chamber/data/me
 async function getDirectoryData() {
     const response = await fetch(directoryDataURL);
     const data = await response.json();
-    console.log(data);
-    // displayDirectory(data);
+    displayDirectory(data.members);
+}
+
+const displayDirectory = (data) => {
+    data.forEach((member) => {
+        const card = document.createElement('section');
+        card.setAttribute('class', 'directory');
+        const memberName = document.createElement('h2');
+        memberName.textContent = member.name;
+        const logo = document.createElement('img');
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `Picture from ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+
+        card.appendChild(memberName);
+        card.appendChild(logo);
+        document.querySelector('main').appendChild(card);       
+    });
 }
 
 getDirectoryData();
