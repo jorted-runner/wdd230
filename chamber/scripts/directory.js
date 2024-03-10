003bc29ce24d72ae458ecd289df8ef7dafb91320
jorted-runner <jorted.runner1996@gmail.com>
2024-03-09 23:39:23 -0600

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,35 @@
 const directoryDataURL = "https://jorted-runner.github.io/wdd230/chamber/data/members.json";
+const mainElmnt = document.querySelector('main');
+const gridButtons = document.querySelector('.gridButton');
+const gridBtn = document.querySelector('#grid');
+const listBtn = document.querySelector('#list');
+
+listBtn.addEventListener("click", () => {
+    const memberInfoList = document.querySelectorAll('.directoryData');
+    mainElmnt.style.gridTemplateColumns = '1fr';
+    memberInfoList.forEach(memberInfo => {
+        memberInfo.classList.add('directoryList');
+        memberInfo.classList.remove('directoryCard');
+    });
+    gridButtons.style.gridColumn = '1';
+});
+
+gridBtn.addEventListener('click', () => {
+    const memberInfoList = document.querySelectorAll('.directoryData');
+    memberInfoList.forEach(memberInfo => {
+        memberInfo.classList.add('directoryCard');
+        memberInfo.classList.remove('directoryList');
+    });
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth > 770) {
+        mainElmnt.style.gridTemplateColumns = '1fr 1fr';
+        gridButtons.style.gridColumn = '1/3';
+    } else {
+        mainElmnt.style.gridTemplateColumns = '1fr';
+    }
+});
+
 
 async function getDirectoryData() {
     const response = await fetch(directoryDataURL);
@@ -9,7 +40,8 @@ async function getDirectoryData() {
 const displayDirectory = (data) => {
     data.forEach((member) => {
         const card = document.createElement('section');
-        card.setAttribute('class', 'directoryCard');
+        card.setAttribute('class', 'directoryData');
+        card.classList.add('directoryCard');
         const memberName = document.createElement('h2');
         memberName.textContent = member.name;
         const logo = document.createElement('img');
